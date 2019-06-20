10fb0f5057d5ac4a49049db232e4243470cf24dc
Johnny Hall <johnny@recipher.co.uk>
2019-06-20 12:41:38 +0100

    Improve sort ux

--- src/modules/app/components/Sort.js
+++ src/modules/app/components/Sort.js
@@ -1,11 +1,11 @@
 import React, { memo } from 'react';
 import PropTypes from 'prop-types';
-import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import ListItemText from'@material-ui/core/ListItemText';
 import ListItemIcon from'@material-ui/core/ListItemIcon';
-import { Menu as MenuIcon, ArrowUpward, ArrowDownward } from '@material-ui/icons';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { usePopupState, bindTrigger, bindMenu } from 'material-ui-popup-state/hooks'
 
@@ -19,6 +19,32 @@ const styles = theme => ({
   blank: {
     opacity: 0,
   },
+  icon: {
+    marginRight: 0,
+  },
+  message: {
+    lineHeight: 1.5,
+    cursor: 'pointer',
+  },
+  text: {
+    paddingLeft: theme.spacing.unit, 
+    verticalAlign: 'top'
+  },
+});
+
+const Selected = memo(({ sort, trigger, onClick, classes }) => {
+  return (
+    <Typography 
+      variant='subtitle1' 
+      component='p' 
+      className={classes.message}
+      onClick={onClick} 
+      {...trigger}
+    >
+      {sort.ascending ? <ExpandLess /> : <ExpandMore />}
+      <span className={classes.text}>{sort.label}</span>
+    </Typography>
+  );
 });
 
 export default withStyles(styles)(memo(({ id, sort, asc, desc, properties, classes }) => {
@@ -27,24 +53,26 @@ export default withStyles(styles)(memo(({ id, sort, asc, desc, properties, class
   const sorts = Object.keys(properties).map(p => ({
     property: p, 
     label: properties[p], 
+    ascending: sort.ascending,
     action: () => sort.ascending ? desc(p) : asc(p),
   }));
 
+  const selected = sorts.find(s => s.property === sort.property);
+  const trigger = sorts.length > 1 ? bindTrigger(popup) : {};
+  const action = sorts.length === 1 ? selected.action : undefined;
+
   return (
     <>
-      <IconButton className={classes.button} variant='contained' color='inherit' 
-        {...bindTrigger(popup)}>
-        <MenuIcon />
-      </IconButton>
+      <Selected sort={selected} classes={classes} trigger={trigger} onClick={action} />
 
       <Menu {...bindMenu(popup)}>
         {sorts.map(({ property, action, label }, index) => 
           <span key={index} className={classes.item} onClick={popup.close}>
             <MenuItem onClick={action}>
-              <ListItemIcon>
+              <ListItemIcon className={classes.icon}>
                 {sort.property === property
-                  ? sort.ascending ? <ArrowUpward /> : <ArrowDownward />
-                  : <ArrowUpward className={classes.blank} />
+                  ? sort.ascending ? <ExpandLess /> : <ExpandMore />
+                  : <ExpandLess className={classes.blank} />
                 }
               </ListItemIcon>
               <ListItemText inset primary={label} />
